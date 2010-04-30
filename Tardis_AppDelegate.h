3589c7d440a674bc54546080eb9844a569f7363b
Mickey <mickey@onodrim.home>
2010-04-29 20:19:44 -0400

    Initial commit of the project Created data model (with location. W00t.)

---
+++ Tardis_AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  Tardis_AppDelegate.h
+//  Tardis
+//
+//  Created by Mickey on 4/29/10.
+//  Copyright MRR Software 2010 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Tardis_AppDelegate : NSObject 
+{
+    NSWindow *window;
+    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;
+}
+
+@property (nonatomic, retain) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:sender;
+
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Tardis
+//
+//  Created by Mickey on 4/29/10.
+//  Copyright MRR Software 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
