aa24fcd3ea8438307dbe5c29437285e32b423284
Matt <matt@mattsapps.com>
2010-04-29 21:05:47 -0400

    Commit Message.

--- Tardis_AppDelegate.m
+++ Tardis_AppDelegate.m
@@ -23,6 +23,7 @@
 
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
     NSString *basePath = ([paths count] > 0) ? [paths objectAtIndex:0] : NSTemporaryDirectory();
+	NSLog(@"OWNED BY MATT!!!!!!!!!");
     return [basePath stringByAppendingPathComponent:@"Tardis"];
 }
 
